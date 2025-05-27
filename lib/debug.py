db6783e5e0668f57c9a4bb6b97031c400c199663
Silva-NK <kiagonsuzanne@gmail.com>
2025-05-27 22:10:43 +0300

    Adds relationship methods to Magazine class as instructed.

--- lib/debug.py
+++ lib/debug.py
@@ -6,57 +6,57 @@ from lib.models.magazine import Magazine
 
 from lib.db.connection import get_connection
 
-# author1 = Author(name="Jane Smith")
-# author1.save()
-# print(f"Author saved with id = {author1.id} and name = '{author1.name}'")
+author1 = Author(name="Jane Smith")
+author1.save()
+print(f"Author saved with id = {author1.id} and name = '{author1.name}'")
 
-# author2 = Author(name="John Doe")
-# author2.save()
-# print(f"Author saved with id = {author2.id} and name = '{author2.name}'")
+author2 = Author(name="John Doe")
+author2.save()
+print(f"Author saved with id = {author2.id} and name = '{author2.name}'")
 
-# author3 = Author(name="J Jonah Jameson")
-# author3.save()
-# print(f"Author saved with id = {author3.id} and name = '{author3.name}'")
+author3 = Author(name="J Jonah Jameson")
+author3.save()
+print(f"Author saved with id = {author3.id} and name = '{author3.name}'")
 
-# author4 = Author(name="Lois Lane")
-# author4.save()
-# print(f"Author saved with id = {author4.id} and name = '{author4.name}'")
-
-
-
-# mag1 = Magazine(name="Daily Planet", category="Developing Stories")
-# mag1.save()
-# print(f"Magazine saved with id = {mag1.id} and name = '{mag1.name}' in category = '{mag1.category}'")
-
-# mag2 = Magazine(name="Daily Bugle", category="Creature Clashes")
-# mag2.save()
-# print(f"Magazine saved with id = {mag2.id} and name = '{mag2.name}' in category = '{mag2.category}'")
-
-# mag3 = Magazine(name="Nat Geo Wild", category="Nature Today")
-# mag3.save()
-# print(f"Magazine saved with id = {mag3.id} and name = '{mag3.name}' in category = '{mag3.category}'")
+author4 = Author(name="Lois Lane")
+author4.save()
+print(f"Author saved with id = {author4.id} and name = '{author4.name}'")
 
 
 
-# article1 = Article(title="When will we actually address pollution?", author_id=1, magazine_id=1)
-# article1.save()
-# print(f"Article saved with id = {article1.id} and title = '{article1.title}' in '{article1.magazine_id}' by '{article1.author_id}'")
+mag1 = Magazine(name="Daily Planet", category="Developing Stories")
+mag1.save()
+print(f"Magazine saved with id = {mag1.id} and name = '{mag1.name}' in category = '{mag1.category}'")
 
-# article2 = Article(title="What will Spiderman ruin next?", author_id=2, magazine_id=2)
-# article2.save()
-# print(f"Article saved with id = {article2.id} and title = '{article2.title}' in '{article2.magazine_id}' by '{article2.author_id}'")
+mag2 = Magazine(name="Daily Bugle", category="Creature Clashes")
+mag2.save()
+print(f"Magazine saved with id = {mag2.id} and name = '{mag2.name}' in category = '{mag2.category}'")
 
-# article2 = Article(title="Spiderman. Saviour or Destroyer?", author_id=3, magazine_id=2)
-# article2.save()
-# print(f"Article saved with id = {article2.id} and title = '{article2.title}' in '{article2.magazine_id}' by '{article2.author_id}'")
+mag3 = Magazine(name="Nat Geo Wild", category="Nature Today")
+mag3.save()
+print(f"Magazine saved with id = {mag3.id} and name = '{mag3.name}' in category = '{mag3.category}'")
 
-# article4 = Article(title="The Masked Streak of Central City?", author_id=3, magazine_id=1)
-# article4.save()
-# print(f"Article saved with id = {article4.id} and title = '{article4.title}' in '{article4.magazine_id}' by '{article4.author_id}'")
 
-# article5 = Article(title="The extinction of Wildlife", author_id=1, magazine_id=3)
-# article5.save()
-# print(f"Article saved with id = {article5.id} and title = '{article5.title}' in '{article5.magazine_id}' by '{article5.author_id}'")
+
+article1 = Article(title="When will we actually address pollution?", author_id=1, magazine_id=1)
+article1.save()
+print(f"Article saved with id = {article1.id} and title = '{article1.title}' in '{article1.magazine_id}' by '{article1.author_id}'")
+
+article2 = Article(title="What will Spiderman ruin next?", author_id=2, magazine_id=2)
+article2.save()
+print(f"Article saved with id = {article2.id} and title = '{article2.title}' in '{article2.magazine_id}' by '{article2.author_id}'")
+
+article2 = Article(title="Spiderman. Saviour or Destroyer?", author_id=3, magazine_id=2)
+article2.save()
+print(f"Article saved with id = {article2.id} and title = '{article2.title}' in '{article2.magazine_id}' by '{article2.author_id}'")
+
+article4 = Article(title="The Masked Streak of Central City?", author_id=3, magazine_id=1)
+article4.save()
+print(f"Article saved with id = {article4.id} and title = '{article4.title}' in '{article4.magazine_id}' by '{article4.author_id}'")
+
+article5 = Article(title="The extinction of Wildlife", author_id=1, magazine_id=3)
+article5.save()
+print(f"Article saved with id = {article5.id} and title = '{article5.title}' in '{article5.magazine_id}' by '{article5.author_id}'")
 
 
 
@@ -110,8 +110,31 @@ from lib.db.connection import get_connection
 
 
 
-# article6 = author4.add_article(mag1, "Know your heroes: Connor Kent. Meet Superman's petri dish son.")
-# print(f"Article saved: {article6.id} - {article6.title}")
+article6 = author4.add_article(mag1, "Know your heroes: Connor Kent. Meet Superman's petri dish son.")
+print(f"Article saved: {article6.id} - {article6.title}")
 
-# article7 = author3.add_article(2, "Inhuman Sightings in Colombia")
-# print(f"Article saved: {article7.id} - {article7.title}")
+article7 = author3.add_article(2, "Inhuman Sightings in Colombia")
+print(f"Article saved: {article7.id} - {article7.title}")
+
+article8 = author3.add_article(2, "How do we move now that hero, Quinten Beck, is gone?")
+print(f"Article saved: {article8.id} - {article8.title}")
+
+
+
+mag = Magazine.find_by_name("Daily Bugle")
+
+print(f"-- Articles in {mag.name} --")
+for article in mag.articles():
+    print(f"{article.id}: {article.title}")
+
+print(f"\n-- Contributors to {mag.name} --")
+for author in mag.contributors():
+    print(f"{author.id}: {author.name}")
+
+print(f"\n-- Titles of all articles in {mag.name} --")
+for title in mag.article_titles():
+    print(f"- {title}")
+
+print(f"\n-- Contributing authors in {mag.name} (more than 2 articles) --")
+for author in mag.contributing_authors():
+    print(f"{author.id}: {author.name}")
--- lib/models/magazine.py
+++ lib/models/magazine.py
@@ -150,3 +150,56 @@ class Magazine:
         conn.close()
 
         return results
+    
+
+    def articles(self):
+        from lib.models.article import Article
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute(""" SELECT * FROM articles WHERE magazine_id = ? """, (self.id,))
+        rows = cursor.fetchall()
+        conn.close()
+        
+        return [Article(id=row[0], title=row[1], author_id=row[2], magazine_id=row[3]) for row in rows]
+
+        
+    def contributors(self):
+        from lib.models.author import Author
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute(""" SELECT DISTINCT a.* FROM authors a JOIN articles ar ON a.id = ar.author_id
+                      WHERE ar.magazine_id = ?
+                      """, (self.id,))
+        
+        rows = cursor.fetchall()
+        conn.close()
+
+        return [Author(id=row[0], name=row[1]) for row in rows]
+    
+
+    def article_titles(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT title FROM articles WHERE magazine_id = ?", (self.id,))
+        rows = cursor.fetchall()
+        conn.close()
+
+        return [row[0] for row in rows]
+    
+
+    def contributing_authors(self):
+        from lib.models.author import Author
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute(""" SELECT a.id, a.name FROM authors a JOIN articles ar ON a.id = ar.author_id
+                       WHERE ar.magazine_id = ? GROUP BY a.id HAVING COUNT(ar.id) > 2
+                       """, (self.id,))
+
+        rows = cursor.fetchall()
+        conn.close()
+
+        return [Author(id=row[0], name=row[1]) for row in rows]
